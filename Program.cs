b40b008b157729b56c6a9bf94c192a1770be9f34
sgca85 <162612350+sgca85@users.noreply.github.com>
2025-05-05 09:28:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿decimal valor, rendaComprovada, valorParcela, maiorParcelaPermitida;
+int quantidadeParcelas;
+
+Console.WriteLine("¨¨¨¨ Análise de Crédito ¨¨¨¨\n");
+
+Console.Write("Valor desejado..........: ");
+valor = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Quantidade de parcelas..: ");
+quantidadeParcelas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Renda comprovada........: ");
+rendaComprovada = Convert.ToDecimal(Console.ReadLine());
+
+valorParcela = valor / quantidadeParcelas;
+maiorParcelaPermitida = rendaComprovada / 100 * 30;
+
+Console.WriteLine();
+
+Console.WriteLine($"Parcela: {valorParcela:C}");
+Console.WriteLine($"Maior parcela permitida: {maiorParcelaPermitida:C}");
+
+Console.WriteLine();
+
+if (valorParcela > maiorParcelaPermitida)
+{
+    Console.WriteLine("Empréstimo não autorizado.");
+}
+else
+{
+    Console.WriteLine("Empréstimo autorizado.");
+}
